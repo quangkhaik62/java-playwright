d3bae4c4e63621d989c0548ba1e70a6565bc2f58
Quang Khai <quangkhaik62@gmail.com>
2025-03-27 21:57:02 +0700

    interact with element

--- src/test/java/com/serenitydojo/playwright/SimpleTestCase.java
+++ src/test/java/com/serenitydojo/playwright/SimpleTestCase.java
@@ -15,12 +15,25 @@ public class SimpleTestCase {
         Page page = browser.newPage();
 
         page.navigate("https://github.com/quangkhaik62/java-playwright");
-
         String title = page.title();
-
         Assertions.assertTrue(title.contains("java-playwright"));
-
         System.out.println(title);
+
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    void interactwithelement() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("https://practicesoftwaretesting.com/");
+        page.locator("#search-query").fill("Pliers");
+        page.locator("button:has-text('Search')").click();
+        int numberofproduct = page.locator(".card").count();
+        Assertions.assertTrue(numberofproduct > 0);
+        System.out.println(numberofproduct);
 
         browser.close();
         playwright.close();
