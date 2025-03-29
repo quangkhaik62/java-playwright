7f673d2b7f2de8bf6ade618820526521d0037078
Quang Khai <quangkhaik62@gmail.com>
2025-03-29 23:23:57 +0700

    update my first test case (module 3)

--- src/test/java/com/serenitydojo/playwright/SimpleTestCase.java
+++ src/test/java/com/serenitydojo/playwright/SimpleTestCase.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 @UsePlaywright
 public class SimpleTestCase {
     /*Playwright playwright;
