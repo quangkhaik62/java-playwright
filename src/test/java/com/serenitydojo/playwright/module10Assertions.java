35e234bf606bc83ca7af6fcf2d1b703d0a6f2213
Quang Khai <quangkhaik62@gmail.com>
2025-04-15 23:12:08 +0700

    Module 9 interacting with fields

--- src/test/java/com/serenitydojo/playwright/module10Assertions.java
+++ src/test/java/com/serenitydojo/playwright/module10Assertions.java
@@ -1,30 +1,44 @@
 package com.serenitydojo.playwright;
 
 import com.microsoft.playwright.*;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+import org.junit.jupiter.api.*;
 
 public class module10Assertions {
-    Playwright playwright;
-    Browser browser;
-    BrowserContext browserContext;
-    Page page;
+    public static Playwright playwright;
+    public static Browser browser;
+    public static BrowserContext browserContext;
+    public static Page page;
+    String Toolshop = "https://practicesoftwaretesting.com/contact";
 
     @BeforeAll
-    public void SetupBrowser(){
+    public static void SetupBrowser(){
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        browserContext = browser.newContext();
+        browserContext = browser.newContext(new Browser.NewContextOptions().setViewportSize(1920,1080));
     }
     @BeforeEach
     public void SetupPage(){
         page = browserContext.newPage();
     }
     @AfterAll
-    public void Teardown(){
+    public static void Teardown(){
         browser.close();
         playwright.close();
     }
+
+    @DisplayName("Assertions-test")
+    @Test
+    void Module10(){
+        page.navigate(Toolshop);
+        page.locator(".btnSubmit").click();
+//        playwright.selectors().setTestIdAttribute("data-test");
+//        PlaywrightAssertions.assertThat(page.getByTestId("first-name-error")).containsText("First name is required");
+        PlaywrightAssertions.assertThat(page.locator("#subject_alert")).containsText("Subject is required");
+
+
+    }
+
+
 
 }
