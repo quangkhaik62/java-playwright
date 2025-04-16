09ed4511bf5bd4b9209e82889d270205a291e006
Quang Khai <quangkhaik62@gmail.com>
2025-04-16 13:50:05 +0700

    modify module 10

--- src/test/java/com/serenitydojo/playwright/module10Assertions.java
+++ src/test/java/com/serenitydojo/playwright/module10Assertions.java
@@ -4,6 +4,10 @@ import com.microsoft.playwright.*;
 import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import org.junit.jupiter.api.*;
 
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class module10Assertions {
     public static Playwright playwright;
     public static Browser browser;
@@ -29,13 +33,23 @@ public class module10Assertions {
 
     @DisplayName("Assertions-test")
     @Test
-    void Module10(){
+    void Module10() throws URISyntaxException {
         page.navigate(Toolshop);
+        page.getByLabel("First name").fill("Kai");
+        page.getByLabel("Last name").fill("Nguyen");
+        page.getByLabel("Email address").fill("Kainguyen@aam1.com");
+//        var webmaster = page.locator("select[id='subject']");
+//        webmaster.selectOption("webmaster");
+        var Messagefield = page.getByLabel("Message");
+        Messagefield.fill("ipc@12IPCKCOI");
+        Path uploadfile = Paths.get(ClassLoader.getSystemResource("testinput.txt").toURI());
+        page.setInputFiles("#attachment", uploadfile);
+        page.waitForTimeout(7000);
         page.locator(".btnSubmit").click();
 //        playwright.selectors().setTestIdAttribute("data-test");
 //        PlaywrightAssertions.assertThat(page.getByTestId("first-name-error")).containsText("First name is required");
+        System.out.println("check assertions");
         PlaywrightAssertions.assertThat(page.locator("#subject_alert")).containsText("Subject is required");
-
 
     }
 
