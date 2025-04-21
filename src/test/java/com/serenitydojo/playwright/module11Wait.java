a0d0a23ddb47143afd012904b123c5452f499bdb
Quang Khai <quangkhaik62@gmail.com>
2025-04-21 22:46:07 +0700

    Init Module 11

---
+++ src/test/java/com/serenitydojo/playwright/module11Wait.java
@@ -0,0 +1,44 @@
+package com.serenitydojo.playwright;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class module11Wait {
+    public static Playwright playwright;
+    public static Browser browser;
+    public static BrowserContext browserContext;
+    Page page;
+
+    @BeforeAll
+    public static void SetupBrowser(){
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                        .setHeadless(false)
+        );
+        browserContext = browser.newContext(
+                new Browser.NewContextOptions()
+                        .setViewportSize(1920,1080)
+        );
+    }
+
+    @BeforeEach
+    void SetupPage(){
+        page = browserContext.newPage();
+        page.navigate("https://practicesoftwaretesting.com/");
+    }
+
+    @AfterAll
+    public static void Teardown(){
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    void Waitfirst(){
+        page.getByLabel("Sort").selectOption("Name (A - Z)");
+    }
+}
