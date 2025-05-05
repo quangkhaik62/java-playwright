4dcd8202bf90cf777043f1062e1edd2c85165b85
Quang Khai <quangkhaik62@gmail.com>
2025-05-05 23:28:54 +0700

    modified Module 11

--- src/test/java/com/serenitydojo/playwright/module11Wait.java
+++ src/test/java/com/serenitydojo/playwright/module11Wait.java
@@ -2,6 +2,7 @@ package com.serenitydojo.playwright;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.assertions.PlaywrightAssertions;
+import com.microsoft.playwright.options.AriaRole;
 import com.microsoft.playwright.options.LoadState;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
@@ -69,5 +70,19 @@ public class module11Wait {
                         .stream().toList();
         page.waitForSelector(".card-img-top", new Page.WaitForSelectorOptions().setTimeout(3000));
         Assertions.assertThat(ProductNames).contains("Claw Hammer");
+    }
+
+    @Test
+    @DisplayName("appear & disappear")
+    void waitAppear(){
+        page.locator(".card-img-top[alt='Claw Hammer']").click();
+        page.waitForLoadState(LoadState.NETWORKIDLE);
+        page.locator("button[id='btn-add-to-cart']").click();
+
+        PlaywrightAssertions.assertThat(page.getByRole(AriaRole.ALERT)).isVisible();
+        PlaywrightAssertions.assertThat(page.getByRole(AriaRole.ALERT)).hasText("Product added to shopping cart.");
+
+        page.waitForCondition( () -> page.getByRole(AriaRole.ALERT).isHidden());
+
     }
 }
